f51406dc90c70ee56e05d94b54d85e26558f3361
patryktk <patrick.tkaczyk@gmail.com>
2019-12-13 01:51:15 +0100

    Login

---
+++ login/src/main/java/pl/yourganizer/login/model/Authority.java
@@ -0,0 +1,22 @@
+package pl.yourganizer.login.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Authority {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+
+    @Column
+    private String authority;
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+}
